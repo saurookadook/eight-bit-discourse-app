72d2e59cee89649eb4efcdd0b77185e7d087f3c6
saurookadook <maskiella@gmail.com>
2018-11-21 19:35:38 -0500

    Setup routes, create migrations, configure rake task for starting servers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
+  scope module: 'api' do 
+    post 'user_token' => 'user_token#create'
+    post 'find_user' => 'users#find'
+    get 'signup' => 'users#new'
+    post 'signup' => 'users#create'
+
+    # resources :users, only: [:show] do
+    #   resources :posts
+    #   resources :comments
+    # end
+
+    resources :posts do
+      resources :comments
+    end
+
+    # resources :users, :posts, :comments do 
+    resources :users do
+      # post 'user_token' => 'user_token#create'
+      # post 'find_user' => 'users#find'
+      resources :posts
+      resources :comments
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
