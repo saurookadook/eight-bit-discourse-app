470d2596754c38e9cf626816c7395b64aa66fcf6
saurookadook <maskiella@gmail.com>
2019-02-25 18:27:06 -0500

    Refactor out some more bootstrap

--- client/src/containers/PostForm.js
+++ client/src/containers/PostForm.js
@@ -55,69 +55,65 @@ class PostForm extends Component {
 
   render() {
     let userLoggedIn = this.props.user ? this.props.user : false;
-
+    
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="OneUp FormUp p-4 my-4">
-          { userLoggedIn ? (
-            <React.Fragment>
-              <h3>Anything to discuss with the hive mind?</h3>
-              <form
-                id="post-form"
-                onSubmit={this.onSubmitHandler}
-              >
-                <p>
-                  <input
-                    className="mr-2"
-                    ref="titleInput"
-                    type="text"
-                    name="title"
-                    placeholder="Title/Topic"
-                    value={this.state.title}
-                    onChange={this.onChangeHandler}
-                  />
-                  <input
-                    className="ml-2"
-                    ref="gameInput"
-                    type="text"
-                    name="game"
-                    placeholder="Game"
-                    value={this.state.game}
-                    onChange={this.onChangeHandler}
-                  />
-                </p>
-                <p>
-                  <textarea
-                    ref="discussionInput"
-                    name="discussion"
-                    className="textarea"
-                    placeholder="Your thoughts...."
-                    value={this.state.discussion}
-                    onChange={this.onChangeHandler}
-                  />
-                </p>
-                <p>
-                  How would you rate this game?
-                </p>
-                <p>
-                  <input
-                    ref="ratingInput"
-                    type="number"
-                    name="rating"
-                    placeholder="Rating (1-10)"
-                    value={this.state.rating}
-                    onChange={this.onChangeHandler}
-                  />
-                </p>
-                <button type="submit">
-                  Add a post
-                </button>
-              </form>
-            </React.Fragment>
-            ) : ('Log in, dummy!') }
-          </div>
-        </div>
+      <div className="OneUp FormUp">
+      { userLoggedIn ? (
+        <React.Fragment>
+          <h3 className="FormUpHeader">Anything to discuss with the hive mind?</h3>
+          <form
+            id="post-form"
+            onSubmit={this.onSubmitHandler}
+          >
+            <p>
+              <input
+                className="mr-2"
+                ref="titleInput"
+                type="text"
+                name="title"
+                placeholder="Title/Topic"
+                value={this.state.title}
+                onChange={this.onChangeHandler}
+              />
+              <input
+                className="ml-2"
+                ref="gameInput"
+                type="text"
+                name="game"
+                placeholder="Game"
+                value={this.state.game}
+                onChange={this.onChangeHandler}
+              />
+            </p>
+            <p>
+              <textarea
+                ref="discussionInput"
+                name="discussion"
+                className="textarea"
+                placeholder="Your thoughts...."
+                value={this.state.discussion}
+                onChange={this.onChangeHandler}
+              />
+            </p>
+            <p>
+              How would you rate this game?
+            </p>
+            <p>
+              <input
+                ref="ratingInput"
+                type="number"
+                name="rating"
+                placeholder="Rating (1-10)"
+                value={this.state.rating}
+                onChange={this.onChangeHandler}
+              />
+            </p>
+            <button type="submit">
+              Add a post
+            </button>
+          </form>
+        </React.Fragment>
+        ) : ('Log in, dummy!') }
       </div>
     )
   }
