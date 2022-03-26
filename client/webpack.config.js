17d6b5c1d061d0e91b56e6507661c5d45a959b0c
Andy Maskiell <maskiella@gmail.com>
2022-03-26 12:27:56 -0400

    Maybe specifying the CSS loaders will resolve the issue?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,53 +6,81 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isProduction = process.env.NODE_ENV == "production";
 
 const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
+    ? MiniCssExtractPlugin.loader
+    : "style-loader";
 
 const config = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-  },
-  resolve: {
-      extensions: [".ts", ".js"]
-  }
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/i,
+            loader: "babel-loader",
+        },
+        {
+            test: /\.css$/i,
+            use: [
+                { loader: stylesHandler },
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: true
+                    }
+                }
+            ],
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                { loader: stylesHandler },
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: true
+                    }
+                },
+                { loader: "sass-loader" }
+            ],
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+            type: "asset",
+        },
+
+        // Add your rules for custom modules here
+        // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    resolveLoader: {
+        modules: [
+            path.join(__dirname, "node_modules")
+        ]
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, "node_modules")
+        ]
+    }
+
+    // resolve: {
+    //     extensions: [".ts", ".js"]
+    // }
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
+    if (isProduction) {
+        config.mode = "production";
 
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = "development";
-  }
-  return config;
+        config.plugins.push(new MiniCssExtractPlugin());
+    } else {
+        config.mode = "development";
+    }
+    return config;
 };
