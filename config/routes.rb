b47a496534241988f3f6badbe81eb27278dbf8e2
Andy Maskiell <maskiella@gmail.com>
2022-03-31 09:50:31 -0400

    Maybe fix weird routing error?

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
 
-  scope 'api' do 
-    post 'user_token' => 'user_token#create'
-    post 'find_user' => 'users#find'
-    get 'signup' => 'users#new'
-    post 'signup' => 'users#create'
+    get '/', to: proc { [200, {}, ['']] }
 
-    resources :posts do
-      resources :comments
+    scope 'api' do
+        post 'user_token' => 'user_token#create'
+        post 'find_user' => 'users#find'
+        get 'signup' => 'users#new'
+        post 'signup' => 'users#create'
+
+        resources :posts do
+            resources :comments
+        end
+
+        # resources :users, :posts, :comments do
+        resources :users do
+            resources :posts
+            resources :comments
+        end
     end
 
-    # resources :users, :posts, :comments do 
-    resources :users do
-      resources :posts
-      resources :comments
+    get '*path', to: "application#fallback_index_html", constraints: -> (request) do
+        !request.xhr? && request.format.html?
     end
-  end
-
-  get '*path', to: "application#fallback_index_html", constraints: -> (request) do
-    !request.xhr? && request.format.html?
-  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
